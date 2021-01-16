de7d31a215f6647666c3a0b4a5ea2fcd06149c00
Alexandra <alexandra95brito@gmail.com>
2021-01-16 17:10:44 +0000

    Popup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { CanvaComponent } from './canva/canva.component';
+import { PopupComponent } from './popup/popup.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     CanvaComponent,
+    PopupComponent,
   ],
   imports: [
     BrowserModule,
-    CommonModule
+    CommonModule,
+    MatDialogModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
